4a66ff8f9e28edd9ae4ac7caea65512da8dadd79
DavidCodes987123 <75763282+DavidCodes987123@users.noreply.github.com>
2021-05-18 17:05:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+
+
+
+}
+
+function setup() {
+
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+
+}
+
+function draw() {}
+
+function takesSnapshot() {
+
+save('picture1.png');
+
+}
