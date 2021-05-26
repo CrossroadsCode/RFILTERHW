2296990990eeff00ce150aa840e0c79d9a1bae44
DavidCodes987123 <75763282+DavidCodes987123@users.noreply.github.com>
2021-05-26 17:56:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function preload() {
-
+clownnose=loadImage('https://i.postimg.cc/X7FMgkp4/clown.png');
 
 
 }
@@ -11,13 +11,43 @@ canvas.center();
 video=createCapture(VIDEO);
 video.size(300, 300);
 video.hide();
-  
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotPoses);
+
 }
 
 function draw() {
-image(video, 0,0,300,300);}
-function takesSnapshot() {
+    image(video, 0,0,300,300);
+  /*  fill(255, 0, 0);
+    stroke(0, 0, 0);
+    circle(nosex, nosey, 20);*/
+    image(clownnose,nosex-15,nosey-15,30,30);
+    
+}
 
-save('picture1.png');
+function takeSnapshot() {
+
+save('HahUrAclown.png');
 
 }
+
+
+function modelLoaded() {
+
+console.log('PoseNet is loaded! Have fun with the filter!');
+
+}
+
+function gotPoses(results) {
+if(results.length>0) {
+
+console.log(results);
+nosex=results[0].pose.nose.x;
+nosey=results[0].pose.nose.y;
+console.log("nosex="+results[0].pose.nose.x);
+console.log("nosey="+results[0].pose.nose.y);
+}}
+
+nosex=0;
+
+nosey=0;
